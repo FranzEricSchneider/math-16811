b665fe91c9683fa4eb1b883d58094511ce1c5cfb
Eric <franz.eric.schneider@gmail.com>
2021-09-03 12:38:44 -0400

    Decompose seems to be working

--- hw1/p1.py
+++ hw1/p1.py
@@ -10,6 +10,9 @@ Simplifications:
 Demonstrate that your implementation works properly on some examples.
 """
 
+# Always
+numpy.set_printoptions(suppress=True, precision=4)
+
 
 EXAMPLES = [
     numpy.array([
@@ -19,19 +22,32 @@ EXAMPLES = [
     ])
 ]
 
+# Compare against THRESHOLD instead of "== 0" to deal with float error
+THRESHOLD = 1e-10
+
+# Whether to include print statements
+DEBUG = True
+
 
 def main():
     for A in EXAMPLES:
-        P, L, D, U = decompose_lu(A.astype(float))
+        P, L, D, U = decompose_pldu(A.astype(float))
+        if DEBUG:
+            print(f"Input A:\n{A}")
+            print(f"Final P:\n{P}")
+            print(f"Final L:\n{L}")
+            print(f"Final D:\n{D}")
+            print(f"Final U:\n{U}")
 
 
-def decompose_lu(A):
-    """TODO."""
+def decompose_pldu(A):
+    """Take a non-singular square matrix and decompose it into PA = LDU."""
 
     # Initial assertions
     assert len(A.shape) == 2
     assert A.shape[0] == A.shape[1]
     assert A.dtype == float
+    assert numpy.linalg.det(A) != 0
 
     # Initialize the matrices
     P = numpy.eye(A.shape[0])
@@ -39,28 +55,84 @@ def decompose_lu(A):
     DU = A.copy()
 
     # Then start modifying
+    active_row = 0
     while not full_upper_triangular(DU):
         start = DU.copy()
 
-        # TODO: Put algorithm here
+        # If the current row is not good (0 on the diagonal), swap rows
+        if row_not_eligible(DU, active_row):
+            best_idx = best_swap(DU, active_row)
+            if DEBUG: print(f"Swapping row{active_row} with row{best_idx}. Starting DU:\n{DU}")
+            P, L, DU = swaprow(P, L, DU, active_row, best_idx)
+            if DEBUG: print(f"Ending DU:\n{DU}")
 
+        # Zero out the column under the diagonal of the active row
+        for row in range(active_row + 1, DU.shape[0]):
+            if abs(DU[row, active_row]) > THRESHOLD:
+                scale = -DU[row, active_row] / DU[active_row, active_row]
+                if DEBUG: print(f"Adding {scale} * row{active_row} with row{row}. Starting DU:\n{DU}")
+                P, L, DU = addrow(P, L, DU, active_row, row, scale)
+                if DEBUG: print(f"Ending DU:\n{DU}")
+
+        # Advance to the next row
+        active_row += 1
+
+        # TODO: Test this assertion with a no-op row. Maybe an empty decomp?
         if numpy.allclose(start, DU):
             raise ValueError("We've gotten into a loop with no progress...")
 
+    # Split D and U
+    values = numpy.diag(DU)
+    D = numpy.diag(values)
+    U = DU.copy()
+    for row, value in enumerate(values):
+        U[row, :] /= value
 
-def full_upper_triangular(DU, threshold=1e-10):
+    return P, L, D, U
+
+
+def full_upper_triangular(DU):
     """
     Returns boolean, true if we have an upper triangular matrix with no zeroes
     on the diagonal.
     """
-    # Boolean, using "> threshold" instead of "== 0" to deal with float error
-    full_diagonal = all(numpy.abs(numpy.diagonal(DU)) > threshold)
+    # Boolean, using "> THRESHOLD" instead of "== 0" to deal with float error
+    full_diagonal = all(numpy.abs(numpy.diagonal(DU)) > THRESHOLD)
 
     # Get a mask of the lower triangular and check that the values there are 0
     mask = numpy.tril(numpy.ones(DU.shape, dtype=bool), k=-1)
-    lower_empty = all(numpy.abs(DU[mask]) < threshold)
+    lower_empty = all(numpy.abs(DU[mask]) < THRESHOLD)
 
     return full_diagonal and lower_empty
+
+
+def row_not_eligible(DU, idx):
+    """Return True if the diagonal for the indicated row is 0."""
+    return abs(DU[idx, idx]) < THRESHOLD
+
+
+def best_swap(DU, active_row):
+    """Return the best swap row for the indicated row.
+
+    NOTE: I've chosen the best row as "the one with the most zeros past the
+    relevant diagonal", since I figure that will reduce chances for requiring
+    future swaps without actually reasoning about future swaps. No idea if this
+    is a good criteria.
+    """
+    print(f"DU: {DU}")
+    print(f"active_row + 1: {active_row + 1}")
+    print(f"DU.shape[0]: {DU.shape[0]}")
+    print(f"range(active_row + 1, DU.shape[0]): {list(range(active_row + 1, DU.shape[0]))}")
+    print(f"active_row: {active_row}")
+    print(f"DU[idx, active_row]: {DU[2, active_row]}")
+    candidates = [idx for idx in range(active_row + 1, DU.shape[0])
+                  if abs(DU[idx, active_row]) > THRESHOLD]
+    assert len(candidates) > 0
+    return sorted(
+        candidates,
+        key=lambda x: sum(numpy.abs(DU[x, active_row:]) < THRESHOLD),
+        reverse=True,
+    )[0]
 
 
 def addrow(P, L, DU, idx0, idx1, scale):
--- hw1/test_p1.py
+++ hw1/test_p1.py
@@ -1,7 +1,13 @@
 import numpy
 import pytest
 
-from p1 import addrow, full_upper_triangular, swaprow
+from p1 import (addrow,
+                best_swap,
+                decompose_pldu,
+                full_upper_triangular,
+                row_not_eligible,
+                swaprow,
+                )
 
 
 @pytest.fixture
@@ -27,6 +33,75 @@ def DU():
         [7, 2, 0, 3],
         [8, 3, 1, 6],
     ], dtype=float)
+
+
+@pytest.mark.parametrize("A", (
+    # Start with really simple stuff
+    numpy.array([[1, 0], [0, 1]]),
+    numpy.array([[-1, 10], [0, 1]]),
+    numpy.eye(4),
+    # Then make some slightly more realistic arrays
+    numpy.array([
+        [  4,   3,   2,   1],
+        [0.5, 1.2, 0.4, -10],
+        [  9,   0,   2, 0.5],
+        [ -5, 0.3,  15,   4],
+    ]),
+    numpy.array([
+        [  4,   3,   2,   1],
+        [  0, 1.2, 0.4, -10],
+        [  0,   0,   2, 0.5],
+        [  0,   0,   0,   4],
+    ]),
+    numpy.array([
+        [  0,   0,   0,   4],
+        [  0,   0,   2, 0.5],
+        [  0, 1.2, 0.4, -10],
+        [  4,   3,   2,   1],
+    ]),
+))
+def test_decompose_pldu(A):
+    P, L, D, U = decompose_pldu(A.astype(float))
+
+    # First, check the overall shape and type
+    assert P.shape == A.shape
+    assert L.shape == A.shape
+    assert D.shape == A.shape
+    assert U.shape == A.shape
+    assert P.dtype == float
+    assert L.dtype == float
+    assert D.dtype == float
+    assert U.dtype == float
+
+    # Then check that matrices have the right "shape", a.k.a. only have values
+    # in the right places
+
+    # P should be only zeros and ones, and should have a single 1 per row
+    assert set(P.flatten()) == {0, 1}
+    assert numpy.sum(P) == A.shape[0]
+
+    # L should be lower triangular, with ones on the diagonal
+    assert numpy.allclose(L, numpy.tril(L))
+    assert numpy.allclose(numpy.diag(L), numpy.ones(A.shape[0]))
+
+    # D should be diagonal, values arbitrary
+    assert numpy.allclose(D, numpy.diag(numpy.diag(D)))
+
+    # U should be upper triangular, with ones on the diagonal
+    assert numpy.allclose(U, numpy.triu(U))
+    assert numpy.allclose(numpy.diag(U), numpy.ones(A.shape[0]))
+
+    # Then do a functional check on some arbitrary vectors
+    x_list = [
+        numpy.array([0, 1, 2, 3, 4, 5, 6][:A.shape[0]]),
+        numpy.array([-5, 2, 3.5, -7.1, 3, 5.5][:A.shape[0]]),
+        numpy.array([1e2, -1e1, 1, -1e-1, 1e-2, -1e-3][:A.shape[0]]),
+    ]
+    for x in x_list:
+        assert numpy.allclose(
+            P @ A @ x,
+            L @ D @ U @ x,
+        )
 
 
 @pytest.mark.parametrize("DU, expected", (
@@ -66,6 +141,91 @@ def DU():
 ))
 def test_full_upper_triangular(DU, expected):
     assert full_upper_triangular(DU) == expected
+
+
+@pytest.mark.parametrize("DU, active_idx, expected", (
+    (
+        numpy.array([
+            [0, 1, 1],
+            [1, 1, 1],
+            [0, 1, 0],
+        ]),
+        0,
+        True,
+    ),
+    (
+        numpy.array([
+            [8, 5, 1],
+            [0, 0, 3],
+            [0, 4, 7],
+        ]),
+        1,
+        True,
+    ),
+    (
+        numpy.array([
+            [8,  5, 1],
+            [0, -4, 7],
+            [0,  0, 3],
+        ]),
+        1,
+        False,
+    ),
+    (
+        numpy.array([
+            [8, 5, 1],
+            [0, 0, 3],
+            [0, 6, 0],
+        ]),
+        2,
+        True,
+    ),
+))
+def test_row_not_eligible(DU, active_idx, expected):
+    assert row_not_eligible(DU, active_idx) == expected
+
+
+@pytest.mark.parametrize("DU, active_idx, expected", (
+    (
+        numpy.array([
+            [0, 1, 1],
+            [1, 1, 1],
+            [0, 1, 0],
+        ]),
+        0,
+        1,
+    ),
+    (
+        numpy.array([
+            [0, 4, 9],
+            [1, 2, 0],
+            [3, 0, 0],
+        ]),
+        0,
+        2,
+    ),
+    (
+        numpy.array([
+            [8, 5, 1, 0],
+            [0, 0, 3, 3],
+            [0, 4, 0, 4],
+            [0, 8, 0, 0],
+        ]),
+        1,
+        3,
+    ),
+    (
+        numpy.array([
+            [ 1,  1, 0],
+            [ 0,  0, 2],
+            [ 0, -2, 3],
+        ]),
+        1,
+        2,
+    ),
+))
+def test_best_swap(DU, active_idx, expected):
+    assert best_swap(DU, active_idx) == expected
 
 
 class TestAddRow:
