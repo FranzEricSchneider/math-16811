deabbaa2031faf8955cbacf139446eaf34f6208c
Eric <franz.eric.schneider@gmail.com>
2021-10-10 13:53:19 -0400

    Started on RANSAC

--- hw3/p4.py
+++ hw3/p4.py
@@ -2,6 +2,8 @@ from matplotlib import pyplot
 from mpl_toolkits import mplot3d
 import numpy
 
+RNG = numpy.random.default_rng(12345)
+
 
 # Always
 numpy.set_printoptions(suppress=True, precision=3)
@@ -14,25 +16,48 @@ def read_values(filename):
                         for line in lines])
 
 
-def p4a(filename):
+def plane_distance(xyz, weights, average=True):
+    """
+    See section 4.1.2 for the equation.
+    xyz should be (N, 3)
+    weights should be (4,)
+    """
 
-    xyz_values = read_values(filename)
-    plot_raw(xyz_values, filename)
+    # This assertion must be true for the plane distance to work
+    assert numpy.isclose(numpy.linalg.norm(weights[:3]), 1.0)
 
-    u, s, vt = numpy.linalg.svd(xyz_values)
-    sigma_inv = numpy.zeros(xyz_values.T.shape)
+    projection = xyz.dot(weights[:3])
+    distance = numpy.abs(projection + weights[3])
+    if average:
+        return numpy.average(distance)
+    else:
+        return distance
+
+
+def fit_plane(xyz):
+    u, s, vt = numpy.linalg.svd(xyz)
+    sigma_inv = numpy.zeros(xyz.T.shape)
     for i, s_value in enumerate(s):
         sigma_inv[i, i] = 1 / s_value
 
     # TODO: Explain
-    d_vector = -1 * numpy.ones(xyz_values.shape[0])
+    d_vector = -1 * numpy.ones(xyz.shape[0])
 
     # Calculate weights, then add in the forced d=1 setting
     weights = vt.T @ sigma_inv @ u.T @ d_vector
     weights = numpy.hstack((weights, [1]))
-    print(f"Weights:\n{weights}")
+    # Normalize around (a, b, c) (the direction vector)
+    weights /= numpy.linalg.norm(weights[:3])
+    return weights
 
+
+def p4a(filename):
+    xyz_values = read_values(filename)
+    plot_raw(xyz_values, filename)
+    weights = fit_plane(xyz_values)
+    print(f"Weights from {filename}: {weights}")
     plot_processed(xyz_values, filename, weights)
+    print(f"Average distance from plane: {plane_distance(xyz_values, weights, average=True) * 1000}mm")
 
 
 def plot_raw(xyz, filename):
@@ -46,14 +71,14 @@ def plot_raw(xyz, filename):
     pyplot.show()
 
 
-def plane(X, Y, weights):
+def plane_z_from_xy(X, Y, weights):
     """
     We're working with the plane equation ax + by + cz + d = 0. Therefore
         -cz = ax + by + d
         z = -(ax + by + d) / c
     """
     a, b, c, d = weights
-    return -(a * X + b * Y + d) / c
+    return -(a*X + b*Y + d) / c
 
 
 def plot_processed(xyz, filename, weights, n=50):
@@ -63,7 +88,7 @@ def plot_processed(xyz, filename, weights, n=50):
     x = numpy.linspace(numpy.min(xyz[:, 0]), numpy.max(xyz[:, 0]), n)
     y = numpy.linspace(numpy.min(xyz[:, 1]), numpy.max(xyz[:, 1]), n)
     X, Y = numpy.meshgrid(x, y)
-    Z = plane(X, Y, weights)
+    Z = plane_z_from_xy(X, Y, weights)
     axis.plot(X.flatten(), Y.flatten(), Z.flatten(), "r", label="Fit")
 
     axis.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], "bo", label="Raw")
@@ -89,5 +114,105 @@ def construct_matrix(xvals, order=5):
     ])
 
 
+def ransac_plane(xyz, min_frac=0.25, batch=12, clean_std=2.07e-3):
+
+    # Choose a threshold that is a certain number of standard deviations from
+    # a clean fit to good data
+    threshold = 3 * clean_std
+    # Make a vector mask to check which variables have already been claimed
+    unclaimed = numpy.ones(xyz.shape[0], dtype=bool)
+    # Number of points needed
+    min_num_fit = min_frac * len(unclaimed)
+    # Models found (weights)
+    models = []
+    # Quality of said models
+    errors = []
+
+    while numpy.sum(unclaimed) > min_num_fit:
+
+        # Make an arbitrary set of weights as a starter set
+        best_model = numpy.array([1, 1, 1, 1])
+        best_error = 1000
+        # These are the indices available for choosing this round
+        unclaimed_indices = numpy.argwhere(unclaimed).flatten()
+        # Check each time whether we founc anything
+        got_new_model = False
+
+        for _ in range(batch):
+            # Select three random points and calculate weights
+            chosen_idx = RNG.choice(unclaimed_indices, size=3, replace=False)
+            chosen = xyz[chosen_idx]
+            weights = fit_plane(chosen)
+            # plot_processed(chosen, None, weights)  # Uncomment to see chosen
+
+            # Check if the plane 1) has enough points and 2) is a better model
+            inlier_mask = plane_distance(xyz[unclaimed], weights, average=False) < threshold
+            if numpy.sum(inlier_mask) > min_num_fit:
+                better_weights = fit_plane(xyz[unclaimed][inlier_mask])
+                better_distances = plane_distance(xyz[unclaimed], better_weights, average=False)
+                better_inlier_mask = better_distances < threshold
+                # TODO: Is std a good error metric? Would avg distance be better?
+                # I feel like avg distance may sort of be covered already by the
+                # threshold
+                error = numpy.std(better_distances[better_inlier_mask])
+                if error < best_error:
+                    best_model = better_weights
+
+        # This indicates that the last search came up empty, to just return
+        # what we already have
+        if numpy.allclose(best_model, 1.0):
+            return models, errors, unclaimed
+
+        # Do some bookkeeping to update unclaimed and save the model
+        final_inlier_mask = plane_distance(xyz, best_model, average=False) < threshold
+        unclaimed &= numpy.logical_not(final_inlier_mask)
+        models.append(best_model)
+        errors.append(best_error)
+
+    return models, errors, unclaimed
+
+
+def p4c(filename):
+
+    xyz_values = read_values(filename)
+    plot_raw(xyz_values, filename)
+
+    weight_groups, _, unclaimed = ransac_plane(xyz_values, min_frac=0.6, batch=12)
+    assert len(weight_groups) == 1
+    weights = weight_groups[0]
+
+    print(f"Weights after RANSAC from {filename}: {weights}")
+    plot_processed(xyz_values, filename, weights)
+    print("Average inlier distance from plane: "
+          f"{plane_distance(xyz_values[numpy.logical_not(unclaimed)], weights, average=True) * 1000}mm")
+    print(f"Average distance from plane: {plane_distance(xyz_values, weights, average=True) * 1000}mm")
+
+
+def p4d(filename):
+
+    xyz_values = read_values(filename)
+    plot_raw(xyz_values, filename)
+
+    weight_groups, _, _ = ransac_plane(xyz_values, min_frac=0.15, batch=100)
+    import ipdb; ipdb.set_trace()
+
+    for weights in weight_groups:
+        print(f"Weights after RANSAC from {filename}: {weights}")
+        plot_processed(xyz_values, filename, weights)
+
+
+
+
+
 if __name__ == "__main__":
-    p4a("clear_table.txt")
+    # Part A
+    # p4a("clear_table.txt")
+
+    # Part B can just use the same code
+    # p4a("cluttered_table.txt")
+
+    # Part C
+    # p4c("cluttered_table.txt")
+
+    # Part D
+    p4d("clean_hallway.txt")
